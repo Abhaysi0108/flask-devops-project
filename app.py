9a8a059edef8429c9bde284c22ceb8769e7dd867
Abhay Singh <abhaysingh0108@gmail.com>
2026-08-16 05:55:01 +0000

    Initial Flask Application

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>DevOps Project</h1>
+    <h2>Hello Abhay Singh 🚀</h2>
+    <p>Flask Application Running Successfully.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
